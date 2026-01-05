59d3e67b4faf42fc946483be3925435656dcf2d5
root <root@DESKTOP-LU5TK22.localdomain>
2026-01-05 17:45:48 -0300

    atualizou site

--- script.js
+++ script.js
@@ -2,9 +2,40 @@ const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
-  // Toggle mobile menu visible
-  document.body.classList.toggle("show-mobile-menu");
+    // Toggle mobile menu visible
+    document.body.classList.toggle("show-mobile-menu");
 });
 
 // Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Iniciar Swiper
+document.addEventListener("DOMContentLoaded", () => {
+    new Swiper(".menu-bag .swiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: ".menu-bag .swiper-button-next",
+            prevEl: ".menu-bag .swiper-button-prev",
+        },
+
+        pagination: {
+            el: ".menu-bag .swiper-pagination",
+            clickable: true,
+        },
+
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
+
