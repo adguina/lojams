a4e4f633b5570e572bb2575e92dbf1b1f57fbeef
root <root@DESKTOP-LU5TK22.localdomain>
2026-01-05 18:04:16 -0300

    atualizou site

--- index copy.php
+++ index copy.php
@@ -105,59 +105,45 @@
             <h2 class="section-title">Vitrine</h2>
             <div class="section-content">
                 <ul class="menu-list">
-                    <li class="menu-item">
-                        <img src="img/B02.png" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+                    <li class="bag swiper-slide">
+                        <img src="./img/B01.png" alt="Bolsa 01" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
-                    <li class="menu-item">
-                        <img src="img/B02.png" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+
+                    <li class="bag swiper-slide">
+                        <img src="./img/B03.png" alt="Bolsa 02" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
-                    <li class="menu-item">
-                        <img src="img/B03.png" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+
+                    <li class="bag swiper-slide">
+                        <img src="./img/B04.png" alt="Bolsa 03" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
-                    <li class="menu-item">
-                        <img src="img/B02.png" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+
+                    <li class="bag swiper-slide">
+                        <img src="./img/B05.png" alt="Bolsa 04" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
-                    <li class="menu-item">
-                        <img src="img/B08.jpg" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+
+                    <li class="bag swiper-slide">
+                        <img src="./img/B06.png" alt="Bolsa 05" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
-                    <li class="menu-item">
-                        <img src="img/B02.png" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+
+                    <li class="bag swiper-slide">
+                        <img src="./img/B06.png" alt="Bolsa 06" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
-                    <li class="menu-item">
-                        <img src="img/B10.jpg" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
-                    </li>
-                    <li class="menu-item">
-                        <img src="img/B09.jpg" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
-                    </li>
-                    <li class="menu-item">
-                        <img src="img/B07.jpg" alt class="menu-image" />
-                        <h3 class="name">Bolsa Monica Sanches</h3>
-                        <p class="text">Bolsa moda femenina e descontraida para todas
-                            ocasioes</p>
+                    <li class="bag swiper-slide">
+                        <img src="./img/B02.png" alt="Bolsa 06" class="user-image">
+                        <h3 class="name">Modelo 2863</h3>
+                        <i class="feedback">"Bolsa Média para o dia a dia"</i>
                     </li>
                 </ul>
             </div>
